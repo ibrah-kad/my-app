fbde4c6e9c544c2497d8d015fb4564f64acb90a1
ibrah-kad <ibrahimmmari01@gmail.com>
2025-03-04 13:43:39 +0300

    my new file

---
+++ src/Body.js
@@ -0,0 +1,18 @@
+
+
+function Button() {
+    return(
+       <button>Submit</button>
+    );
+
+}
+
+export default function loginfrom(){
+    return(
+        <div>
+            <h1>Welcome to my App</h1>
+            <Button/>
+        </div>
+    );
+}
+
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+ 
+
+ function Header () {
+    return(
+        <>
+        <h1>Welcome to my trial Website</h1>
+        <p> this is my website that am making for trials i want to learn more about it</p>
+        </>
+    );
+ }
+
+ export default Header;
